e3172bd9d659aacde5850beb7ae5fe7c25343d74
joerdav <joe.davidson.21111@gmail.com>
2023-11-02 11:03:06 +0000

    feat: add changelog support

---
+++ README.md
@@ -0,0 +1,8 @@
+# Templ Jetbrains
+
+<!-- Plugin description -->
+Support for the [Templ Programming Language](https://templ.guide/)
+
+## Features
+- Basic LSP Support
+<!-- Plugin description end -->
--- build.gradle.kts
+++ build.gradle.kts
@@ -41,8 +41,22 @@ tasks {
     }
 
     patchPluginXml {
+        version.set(properties("pluginVersion"))
         sinceBuild.set("222")
         untilBuild.set("232.*")
+
+        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
+        pluginDescription.set(providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
+            val start = "<!-- Plugin description -->"
+            val end = "<!-- Plugin description end -->"
+            with(it.lines()) {
+                if (!containsAll(listOf(start, end))) {
+                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                }
+                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
+            }
+        })
+
 
         val changelog = project.changelog
 
