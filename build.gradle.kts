97e1509fbecc497fce4118c1fdc98d723774c6a2
joerdav <joe.davidson.21111@gmail.com>
2024-01-31 10:55:43 +0000

    feat: Only define min versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,6 @@ tasks {
     patchPluginXml {
         version = properties("pluginVersion")
         sinceBuild = properties("pluginSinceBuild")
-        untilBuild = properties("pluginUntilBuild")
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
         pluginDescription = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
